350c63a8cb47a005ceecc85ab200c1b4d4770ee8
sirkells <kelechi_igbokwe@yahoo.com>
2019-03-10 20:19:54 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
 class App extends Component {
+  state = {
+    displayBio: false,
+    name: "Visitor"
+  };
+
+  // use arrow functions to access this
+  toggleBio = () => {
+    this.setState({ displayBio: !this.state.displayBio });
+  };
   render() {
+    // destructuring state to avoid always usimg this.state
+    const { displayBio } = this.state;
+    const bio = displayBio ? (
+      <div>
+        <p>I live in Siegburg, Germany and i code Everyday</p>
+        <p>I enjoy working with Javascript and Python </p>
+        <p>
+          For Javascript i use React and Vue while for Python i use Django and
+          Flask
+        </p>
+        <button onClick={this.toggleBio}>Show less</button>
+      </div>
+    ) : (
+      <div>
+        <button onClick={this.toggleBio}>Read more</button>
+      </div>
+    );
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>Hello!</h1>
+        <p>My name is Kelechi and i am a Software Engineer</p>
+        <p>I have great desire to work on innovative and exciting projects</p>
+        {bio}
       </div>
     );
   }
